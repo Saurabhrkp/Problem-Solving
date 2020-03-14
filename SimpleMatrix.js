c2c7966a1ec690a2f930e66b8b818b06ac4a06c9
Saurabh Patel <38186468+Saurabhrkp@users.noreply.github.com>
2020-03-14 17:12:25 +0530

    Initial Commit

---
+++ SimpleMatrix.js
@@ -0,0 +1,26 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Enter the value of row of matrix? ', data => {
+  const row = Number(data);
+  rl.question('Enter the value of column of matrix? ', data => {
+    const column = Number(data);
+    const matrix = createMatrix(row, column);
+    console.log(matrix);
+  });
+});
+
+function createMatrix(row, column) {
+  var matrix = [];
+  for (var i = 0; i < row; i++) {
+    matrix[i] = [];
+    for (var j = 0; j < column; j++) {
+      matrix[i][j] = i + j;
+    }
+  }
+  return matrix;
+}
