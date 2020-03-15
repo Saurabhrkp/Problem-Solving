b266819e3256e8388427d266d0cc2d77d4b6edda
Saurabh Patel <38186468+Saurabhrkp@users.noreply.github.com>
2020-03-15 15:19:39 +0530

    Added Program to return product of array expect on i

---
+++ Product of Array expect on i/ProductInArray.js
@@ -0,0 +1,30 @@
+const prompt = require('prompt-sync')({ sigint: true });
+var array = [];
+const len = prompt('Enter length of array: ');
+for (var i = 0; i < len; i++) {
+  value = prompt(`Enter value for ${i + 1}: `);
+  array[i] = Number(value);
+}
+console.log(array);
+const productArray = (arr, len) => {
+  if (len == 1) {
+    process.stdout.write(`0`);
+    return;
+  }
+  var product = [];
+  var temp = 1;
+  for (var j = 0; j < len; j++) product[j] = 1;
+  for (i = 0; i < len; i++) {
+    product[i] = temp;
+    temp *= arr[i];
+  }
+  temp = 1;
+  for (i = len - 1; i >= 0; i--) {
+    product[i] *= temp;
+    temp *= arr[i];
+  }
+  for (i = 0; i < len; i++);
+  return product;
+};
+process.stdout.write(`The product array is : `);
+console.log(productArray(array, len));
